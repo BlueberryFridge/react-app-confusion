762dae2b8500cea039e1c6ff42f72c379ea91dcc
Janine Lapiz <jglapiz@up.edu.ph>
2020-10-26 20:58:58 +0800

    Added react-router-dom and added links in footer and header; added routes

--- src/components/MainComponent.js
+++ src/components/MainComponent.js
@@ -1,5 +1,7 @@
 import React, { Component } from 'react';
+import { Switch, Route, Redirect } from 'react-router-dom';
 import Menu from './MenuComponent';
+import Home from './HomeComponent';
 import DishDetail from './DishDetailComponent';
 import { DISHES } from '../shared/dishes';
 import Header from './HeaderComponent';
@@ -11,8 +13,7 @@ class Main extends Component {
   constructor(props){
     super(props);
     this.state = {
-      dishes: DISHES,
-      selectedDish: null
+      dishes: DISHES
     }
   }
 
@@ -21,13 +22,16 @@ class Main extends Component {
   }
 
   render(){
+    const HomePage = () => <Home />;
 
     return (
       <div>
           <Header />
-          <Menu dishes={this.state.dishes}
-                onClick={dishId => this.onDishSelect(dishId)} />
-          <DishDetail dish={this.state.dishes.filter(dish => dish.id === this.state.selectedDish)[0]} />  
+          <Switch>
+            <Route path='/home' component={HomePage} />
+            <Route exact path='/menu' component={() => <Menu dishes={this.state.dishes} />} />
+            <Redirect to='/home' />
+          </Switch>
           <Footer />
       </div>
     );}
