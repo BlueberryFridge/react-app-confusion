3d45d081b2ac33d94ab3a0f76909e06cb875f837
Janine Lapiz <jglapiz@up.edu.ph>
2020-10-27 03:53:39 +0800

    Added AboutComponent.js

--- src/components/MainComponent.js
+++ src/components/MainComponent.js
@@ -1,15 +1,19 @@
 import React, { Component } from 'react';
 import { Switch, Route, Redirect } from 'react-router-dom';
-import Menu from './MenuComponent';
+
+import Header from './HeaderComponent';
 import Home from './HomeComponent';
+import About from './AboutComponent';
+import Menu from './MenuComponent';
 import Contact from './ContactComponent';
+import Footer from './FooterComponent';
+
 import DishDetail from './DishDetailComponent';
 import { DISHES } from '../shared/dishes';
 import { COMMENTS } from '../shared/comments';
 import { PROMOTIONS } from '../shared/promotions';
 import { LEADERS } from '../shared/leaders';
-import Header from './HeaderComponent';
-import Footer from './FooterComponent';
+
 
 // this is a container component
 
@@ -36,6 +40,7 @@ class Main extends Component {
           <Header />
           <Switch>
             <Route path='/home' component={HomePage} />
+            <Route exact path='/aboutus' component={() => <About leaders={this.state.leaders} />} />
             <Route exact path='/menu' component={() => <Menu dishes={this.state.dishes} />} />
             <Route path path='/menu/:dishId' component={DishWithId} />
             <Route exact path='/contactus' component={Contact} />
